908a735f88d7e200edd6841e49d25b535f21dc5b
San <sanuel@live.co.uk>
2023-12-12 21:46:14 +0000

    data folder to store menuitems, posts etc., fix layout with post page view

---
+++ src/data/posts.js
@@ -0,0 +1,8 @@
+const posts = [
+    { id: 1, title: 'Get online week', excerpt: 'Lorem ipsum dolor sit amet', url: 'get-online-week'},
+    { id: 2, title: 'Learn my way', excerpt: 'Lorem ipsum dolor sit amet', url: 'learn-my-way' },
+    { id: 3, title: 'Make it click', excerpt: 'Lorem ipsum dolor sit amet', url: 'make-it-click' },
+    { id: 4, title: 'Digital you', excerpt: 'Lorem ipsum dolor sit amet', url: 'digital-you' },
+]
+
+export default posts;
